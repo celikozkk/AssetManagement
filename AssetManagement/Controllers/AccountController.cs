bb3dc58ba212f6c6793ebefc5d914231230dfa1a
Ozkan Celik <celikozkk@gmail.com>
2023-10-09 02:19:08 +0300

    add AccountController and endpoint for getting positions by account id.

---
+++ AssetManagement/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using AssetManagement.Data;
+using AssetManagement.Dtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetManagement.Controllers;
+
+[ApiController]
+[Route("api/accounts")]
+public class AccountController : ControllerBase
+{
+    private readonly TradingContext _context;
+    private readonly IMapper _mapper;
+
+    public AccountController(TradingContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    // GET: api/accounts/{id}/positions
+    [HttpGet("{id}/positions")]
+    public async Task<ActionResult<IEnumerable<PositionDto>>> GetPositionsByAccountId(int id)
+    {
+        var account = await _context.Accounts.FindAsync(id);
+        if (account == null)
+        {
+            return NotFound("Account not found.");
+        }
+
+        var positions = await _context.Positions.Where(p => p.AccountId == account.Id).ToListAsync();
+
+        return Ok(_mapper.Map<List<PositionDto>>(positions));
+    }
+}
